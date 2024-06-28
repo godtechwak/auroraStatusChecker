5da973402ee03246f603f8d87a2284b133bdc137
Silver.Lee <68718384+godtechwak@users.noreply.github.com>
2024-06-28 16:11:38 +0900

    Create cluster.go

---
+++ rds_util/cluster.go
@@ -0,0 +1,52 @@
+package rds_util
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rds"
+)
+
+type Cluster struct{}
+
+func (c Cluster) Describe(svc *rds.RDS, name string) ([]string, error) {
+	input := &rds.DescribeDBClustersInput{
+		DBClusterIdentifier: aws.String(name),
+	}
+
+	result, err := svc.DescribeDBClusters(input)
+	if err != nil {
+		return nil, err
+	}
+
+	clusterInfo := result.DBClusters[0]
+	clusterParam := result.DBClusters[0].DBClusterMembers[0]
+
+	return []string{
+		*clusterInfo.DBClusterIdentifier,
+		*clusterInfo.EngineVersion,
+		*clusterInfo.Status,
+		*clusterParam.DBClusterParameterGroupStatus,
+	}, nil
+}
+
+func (c Cluster) GetHeaders() []string {
+	return []string{"Time", "Duration", "Cluster Name", "Version", "Status", "Param Status"}
+}
+
+func (c Cluster) SaveListToFile(svc *rds.RDS, fileName string) error {
+	input := &rds.DescribeDBClustersInput{}
+	result, err := svc.DescribeDBClusters(input)
+	if err != nil {
+		return err
+	}
+
+	var names []string
+	for _, cluster := range result.DBClusters {
+		names = append(names, *cluster.DBClusterIdentifier)
+	}
+
+	content := strings.Join(names, "\n")
+	return ioutil.WriteFile(fileName, []byte(content), 0644)
+}
