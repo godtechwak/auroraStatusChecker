610571f8418352c851a12cb41157326481d9076b
Silver.Lee <68718384+godtechwak@users.noreply.github.com>
2024-06-28 16:12:05 +0900

    Create instance.go

---
+++ rds_util/instance.go
@@ -0,0 +1,52 @@
+package rds_util
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rds"
+)
+
+type Instance struct{}
+
+func (i Instance) Describe(svc *rds.RDS, name string) ([]string, error) {
+	input := &rds.DescribeDBInstancesInput{
+		DBInstanceIdentifier: aws.String(name),
+	}
+
+	result, err := svc.DescribeDBInstances(input)
+	if err != nil {
+		return nil, err
+	}
+
+	instanceInfo := result.DBInstances[0]
+	instanceParam := result.DBInstances[0].DBParameterGroups[0]
+
+	return []string{
+		*instanceInfo.DBInstanceIdentifier,
+		*instanceInfo.EngineVersion,
+		*instanceInfo.DBInstanceStatus,
+		*instanceParam.ParameterApplyStatus,
+	}, nil
+}
+
+func (i Instance) GetHeaders() []string {
+	return []string{"Time", "Duration", "Instance Name", "Version", "Status", "Param Status"}
+}
+
+func (i Instance) SaveListToFile(svc *rds.RDS, fileName string) error {
+	input := &rds.DescribeDBInstancesInput{}
+	result, err := svc.DescribeDBInstances(input)
+	if err != nil {
+		return err
+	}
+
+	var names []string
+	for _, instance := range result.DBInstances {
+		names = append(names, *instance.DBInstanceIdentifier)
+	}
+
+	content := strings.Join(names, "\n")
+	return ioutil.WriteFile(fileName, []byte(content), 0644)
+}
